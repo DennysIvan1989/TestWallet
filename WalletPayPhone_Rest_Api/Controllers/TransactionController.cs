71473c1e0dedbb1af2dce248a6d2f86bcb4c23ab
Dennys Moyon <dennysmoyon@outlook.com>
2025-07-29 02:41:38 -0500

    init controller

---
+++ WalletPayPhone_Rest_Api/Controllers/TransactionController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WalletPayPhone_Rest_Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/transaction")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+       
+
+    }
+}
